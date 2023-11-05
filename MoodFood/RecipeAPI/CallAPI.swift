8c826ebfeb19e8b69dd65804103b5f3af9b8eecd
jsinha <jaisinha219@gmail.com>
2023-11-05 03:12:52 -0600

    callAPI and recipe struct finally working fully god bless

--- MoodFood/RecipeAPI/CallAPI.swift
+++ MoodFood/RecipeAPI/CallAPI.swift
@@ -7,54 +7,40 @@
 
 import Foundation
 
-class CallAPI : ObservableObject {
-    @Published var globalRecipe = Recipe()
-    func callAPI(searchString: String) {
-        if let url = URL(string: "http://10.139.251.103:5000/\(searchString)") {
+func callAPI(searchString: String) -> Recipe? {
+    var globalRecipe = Recipe()
+    print("Starting")
+    let group = DispatchGroup()
+    group.enter()
+    if let url = URL(string: "http://10.139.251.103:5000/\(searchString)") {
+        print("Fetched")
+        DispatchQueue.global(qos: .default).async {
             let task = URLSession.shared.dataTask(with: url) { data, response, error in
                 if let error = error {
-                    print("Client error: \(error)")
-                    return
-                }
-                guard let httpResponse = response as? HTTPURLResponse,
-                      (200...299).contains(httpResponse.statusCode) else {
-                    print("Server error")
-                    return
-                }
-                if let mimeType = httpResponse.mimeType, mimeType == "application/json", // Assuming the response is JSON
-                   let data = data {
+                    print("Error: \(error)")
+                } else if let data = data {
+                    // Parse the JSON response if applicable
                     do {
-                        if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                            // Successfully parsed JSON
-                            print("Parsed JSON")
-                            if let name = json["name"] as? String,
-                                let url = json["url"] as? String,
-                                let image = json["image"] as? String,
-                                let ingredients = json["ingredients"] as? [String],
-                                let steps = json["steps"] as? [String],
-                                let prep_time = json["prep_time"] as? Int,
-                                let cook_time = json["cook_time"] as? Int,
-                                let total_time = json["total_time"] as? Int,
-                                let servings = json["servings"] as? Int {
-                                    let recipe = Recipe(name: name, url: url, image: image, ingredients: ingredients,
-                                                        steps: steps, prep_time: prep_time, cook_time: cook_time,
-                                                        total_time: total_time, servings: servings)
-                                
-                                // Now you have your object with JSON data assigned
-                                    self.globalRecipe = recipe
-                                    print("Stored recipe")
-                            }
-                        }
+                        let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+                        // Handle the JSON data here
+                        print("Received JSON data: \(String(describing: json))")
+                        let recipe = try JSONDecoder().decode(Recipe.self, from: data)
+                        globalRecipe = recipe
+                        print("Stored recipe")
+                        
                     } catch {
-                        print("JSON parsing error")
+                        print("Error parsing JSON: \(error)")
                     }
                 }
+                else {
+                    print("Invalid URL")
+                }
+                group.leave()
             }
             task.resume()
-        } else {
-            print("Invalid URL")
         }
-        
+        group.wait()
     }
-
+    print("Recipe name: \(globalRecipe.name)")
+    return globalRecipe
 }
--- MoodFood/RecipeAPI/RecipeObject.swift
+++ MoodFood/RecipeAPI/RecipeObject.swift
@@ -7,27 +7,33 @@
 
 import Foundation
 
-class Recipe {
+struct Recipe: Decodable {
     var name = ""
     var url = ""
     var image = ""
     var ingredients = [String]()
     var steps = [String]()
-    var prep_time = 0
-    var cook_time = 0
-    var total_time = 0
-    var servings = 0
+    var prep_time = ""
+    var cook_time = ""
+    var total_time = ""
+    var servings = ""
     
     init() {}
-    init(name: String, url: String, image: String, ingredients: [String], steps: [String], prep_time: Int, cook_time: Int, total_time: Int, servings: Int) {
-        self.name = name
-        self.url = url
-        self.image = image
-        self.ingredients = ingredients
-        self.steps = steps
-        self.prep_time = prep_time
-        self.cook_time = cook_time
-        self.total_time = total_time
-        self.servings = servings
+    enum RootKeys: String, CodingKey {
+        case name, url, ingredients, steps, prep_time, cook_time, total_time, nb_servings
     }
+      
+     
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: RootKeys.self)
+        name = try values.decode(String.self, forKey: .name)
+        url = try values.decode(String.self, forKey: .url)
+//        image = try values.decode(String.self, forKey: .image)
+        ingredients = try values.decode([String].self, forKey: .ingredients)
+        steps = try values.decode([String].self, forKey: .steps)
+        prep_time = try values.decode(String.self, forKey: .prep_time)
+        cook_time = try values.decode(String.self, forKey: .cook_time)
+        total_time = try values.decode(String.self, forKey: .total_time)
+        servings = try values.decode(String.self, forKey: .nb_servings)
+     }
 }
